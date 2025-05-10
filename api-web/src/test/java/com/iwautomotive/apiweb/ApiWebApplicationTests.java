03d61b6f66758527e2e7cfa77dd6fc0a5f1e5455
ingkoon <ing97220@naver.com>
2025-05-10 19:41:47 +0900

    init project

---
+++ api-web/src/test/java/com/iwautomotive/apiweb/ApiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iwautomotive.apiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
